287d28ae00bb922174fbbb71e2cf3a8395331242
Alyssa Esparza <alyssa.esparza95@gmail.com>
2022-11-09 19:21:29 -0800

    init commit

---
+++ routes/api/thought-route.js
@@ -0,0 +1,29 @@
+const router = required('express').Router();
+
+const { 
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removerReaction,
+} = require('../../controllers/thought-controller');
+
+// api/thoughts
+router.route('/').get(getAllThought).post(createThought);
+
+//api/thoughts/:id
+router.route('/:id');
+router.get(getThoughtById);
+router.put(updateThought);
+router.delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reaction').post(addReaction);
+
+// api/thoughts/:thoughtIs/reactions/:reactionId
+router.route('/:thoughtId/reaction/:reactionId').delete(removeReaction);
+
+module.exports = router;
+
---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// api/user
+router.route('/').get(getAllUser).post(createUser);
+
+//api/user/:id
+router.route('/:id').get(getUserById).post(updateUser).delete(deleteUser);
+
+// api/user/:userId/friends/:friendId
+router.route('/userId/friend/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
